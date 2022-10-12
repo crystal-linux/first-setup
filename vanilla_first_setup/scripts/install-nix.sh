2eef87a5abcae290a159527fa0902b8281651057
axtloss <axtlos@tar.black>
2022-10-12 14:20:57 +0200

    Run nix commands with sudo -u

--- vanilla_first_setup/scripts/install-nix.sh
+++ vanilla_first_setup/scripts/install-nix.sh
@@ -2,5 +2,5 @@
 pacman -S --noconfirm nix
 systemctl enable --now nix-daemon.service
 usermod -aG nix-users $(id -nu $PKEXEC_UID)
-su $(id -nu $PKEXEC_UID) -c 'nix-channel --add https://nixos.org/channels/nixpkgs-unstable'
-su $(id -nu $PKEXEC_UID) -c 'nix-channel --update'
+sudo -u $(id -nu $PKEXEC_UID) 'nix-channel --add https://nixos.org/channels/nixpkgs-unstable'
+sudo -u $(id -nu $PKEXEC_UID) 'nix-channel --update'
