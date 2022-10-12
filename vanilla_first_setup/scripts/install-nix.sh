a950123d52a9eb58731ddc9b0478338618f82813
axtloss <axtlos@tar.black>
2022-10-12 13:54:51 +0200

    Add user to nix-users group in nix install

--- vanilla_first_setup/scripts/install-nix.sh
+++ vanilla_first_setup/scripts/install-nix.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 pacman -S --noconfirm nix
 systemctl enable --now nix-daemon.service
+usermod -aG nix-users $(id -nu $PKEXEC_UID)
 su $(id -nu $PKEXEC_UID) -c 'nix-channel --add https://nixos.org/channels/nixpkgs-unstable'
 su $(id -nu $PKEXEC_UID) -c 'nix-channel --update'
