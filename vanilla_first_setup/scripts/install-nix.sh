14c77387de64411e3058bae0a2cfe76c0913d8ef
axtloss <axtlos@tar.black>
2022-10-12 14:23:38 +0200

    Sudo is weird

--- vanilla_first_setup/scripts/install-nix.sh
+++ vanilla_first_setup/scripts/install-nix.sh
@@ -2,5 +2,5 @@
 pacman -S --noconfirm nix
 systemctl enable --now nix-daemon.service
 usermod -aG nix-users $(id -nu $PKEXEC_UID)
-sudo -u $(id -nu $PKEXEC_UID) 'nix-channel --add https://nixos.org/channels/nixpkgs-unstable'
-sudo -u $(id -nu $PKEXEC_UID) 'nix-channel --update'
+sudo -u $(id -nu $PKEXEC_UID) nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+sudo -u $(id -nu $PKEXEC_UID) nix-channel --update
